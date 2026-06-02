9025a7a3d8d70af8180f3049d039d6c9907da92f
jammm <nikitacirskov0@gmail.com>
2026-06-02 11:57:02 +0300

    Update main_menu.py

--- locales/en/main_menu.py
+++ locales/en/main_menu.py
@@ -33,7 +33,7 @@ TEXTS = {
         "┋\n"
         "┣ Please select your preferred language\n"
         "┋\n"
-        "┗┅┅┅/ Select an option: /"
+        "┗┅┅┅/ <b>Select an option:</b> /"
     ),
     "bot_ready": (
         "<tg-emoji emoji-id=\"5273741156792951269\">🤓</tg-emoji> <b>The bot is ready to work!</b>\n\n"
