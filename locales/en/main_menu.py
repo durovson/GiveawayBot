523bf748e0640b4439b1aeca2b6d9c3381b43984
jammm <nikitacirskov0@gmail.com>
2026-06-02 15:17:05 +0300

    Update main_menu.py

--- locales/en/main_menu.py
+++ locales/en/main_menu.py
@@ -21,24 +21,27 @@ TEXTS = {
     "support_btn": "Support",
     "language_btn": "Language",
     "setup_success": (
-        "<tg-emoji emoji-id=\"5258501105293205250\">👏</tg-emoji> Group <b>{title}</b> successfully registered!\n\n"
-        "<blockquote>Now you can create giveaways in it via private messages with the bot.</blockquote>"
+        "┏┅<tg-emoji emoji-id=\"5258501105293205250\">👏</tg-emoji> Group <b>{title}</b> successfully registered!\n"
+        "┋\n"
+        "┗┅┅┅/ <b>Now you can create giveaways in it via private messages with the bot</b> /"
     ),
     "setup_admin_only": "<tg-emoji emoji-id=\"5273876254989246882\">🤬</tg-emoji> <b>This command can only be executed by a group administrator.</b>",
     "select_group_giveaway": "Select a group to hold the giveaway:",
     "no_groups_available": "<tg-emoji emoji-id=\"5273876254989246882\">🤬</tg-emoji> <b>There are no available groups. Add the bot to the group and make it an administrator.</b>",
     "no_admin_rights": "❌ You do not have administrator rights in connected chats.",
     "select_language": (
-        "┏┅<tg-emoji emoji-id=\"5260512129240276089\">📚</tg-emoji>┅ / Language /\n"
+        "┏┅<tg-emoji emoji-id=\"5260512129240276089\">📚</tg-emoji>┅ /<b>Language</b> /\n"
         "┋\n"
         "┣ Please select your preferred language\n"
         "┋\n"
         "┗┅┅┅/ <b>Select an option:</b> /"
     ),
     "bot_ready": (
-        "<tg-emoji emoji-id=\"5273741156792951269\">🤓</tg-emoji> <b>The bot is ready to work!</b>\n\n"
-        "<blockquote>The group has been automatically registered. Now you can create giveaways via private messages with the bot.</blockquote>\n\n"
-        "<i>If the group does not appear in the list, use the command /setup</i>"
+        "┏┅<tg-emoji emoji-id=\"5273741156792951269\">🤓</tg-emoji>┅ / <b>The bot is ready to work!</b> /\n"
+        "┋\n"
+        "┣ The group has been automatically registered. Now you can create giveaways via private messages with the bot.\n"
+        "┋\n"
+        "┗┅┅┅/ <b>If the group does not appear in the list, use the command /setup</b> /"
     ),
     "otc_not_available": "❌ OTC not available"
 }
