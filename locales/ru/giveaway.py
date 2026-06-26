5d44b7f394684b493d66b6056ea915f82350f236
jammm <nikitacirskov0@gmail.com>
2026-06-26 10:05:15 +0300

    Update giveaway.py

--- locales/ru/giveaway.py
+++ locales/ru/giveaway.py
@@ -1,6 +1,6 @@
 TEXTS = {
     "giveaway_title": (
-        "┏┅<tg-emoji emoji-id=\"5235695112419303615\">🎁</tg-emoji>┅ / <b>Новый розыгрыш</b> /\n"
+        "┏┅<tg-emoji emoji-id=\"5235695112419303615\">🎁</tg-emoji>┅/ <b>Новый розыгрыш</b> /\n"
         "┋\n"
         "┣ Выберите тип розыгрыша.\n"
         "┋\n"
@@ -11,19 +11,19 @@ TEXTS = {
     "giveaway_back_btn": "НАЗАД",
     "giveaway_main_menu_btn": "ГЛАВНОЕ МЕНЮ",
     "giveaway_enter_title": (
-        "┏┅<tg-emoji emoji-id=\"5778299625370817409\">📝</tg-emoji>┅ / <b>Название розыгрыша</b> /\n"
+        "┏┅<tg-emoji emoji-id=\"5778299625370817409\">📝</tg-emoji>┅/ <b>Название розыгрыша</b> /\n"
         "┋\n"
         "┗┅┅┅/ <b>Введите название вашего розыгрыша</b> /"
     ),
     "giveaway_enter_channels": (
-        "┏┅<tg-emoji emoji-id=\"5258185631355378853\">📢</tg-emoji>┅ / <b>Каналы</b> /\n"
+        "┏┅<tg-emoji emoji-id=\"5258185631355378853\">📢</tg-emoji>┅/ <b>Каналы</b> /\n"
         "┋\n"
         "┣ Отправьте ссылки на каналы, на которые должны подписаться пользователи.\n"
         "┋\n"
         "┗┅┅┅/ <b>Пример:</b> @channel1 https://t.me/channel2 /"
     ),
     "giveaway_bot_not_admin": (
-        "┏┅<tg-emoji emoji-id=\"5273876254989246882\">🤬</tg-emoji>┅ / <b>Бот не является администратором в следующих каналах:</b> /\n"
+        "┏┅<tg-emoji emoji-id=\"5273876254989246882\">🤬</tg-emoji>┅/ <b>Бот не является администратором в следующих каналах:</b> /\n"
         "┋\n"
         "{channels}\n"
         "┋\n"
@@ -31,7 +31,7 @@ TEXTS = {
     ),
     "giveaway_i_added_btn": "Я ДОБАВИЛ!",
     "giveaway_select_type": (
-        "┏┅<tg-emoji emoji-id=\"5850317551090800862\">⏳</tg-emoji>┅ / <b>Тип розыгрыша</b> /\n"
+        "┏┅<tg-emoji emoji-id=\"5850317551090800862\">⏳</tg-emoji>┅/ <b>Тип розыгрыша</b> /\n"
         "┋\n"
         "┣ Выберите, как будет определен победитель.\n"
         "┋\n"
@@ -40,12 +40,12 @@ TEXTS = {
     "giveaway_timed_btn": "ОПРЕДЕЛЕННОЕ ВРЕМЯ",
     "giveaway_limited_btn": "ПО КОЛИЧЕСТВУ УЧАСТНИКОВ",
     "giveaway_enter_time": (
-        "┏┅<tg-emoji emoji-id=\"5850317551090800862\">⏰</tg-emoji>┅ / <b>Время завершения</b> /\n"
+        "┏┅<tg-emoji emoji-id=\"5850317551090800862\">⏰</tg-emoji>┅/ <b>Время завершения</b> /\n"
         "┋\n"
         "┗┅┅┅/ <b>Формат:</b> 18:00 /"
     ),
     "giveaway_enter_participants": (
-        "┏┅<tg-emoji emoji-id=\"6032594876506312598\">👥</tg-emoji>┅ / <b>Лимит участников</b> /\n"
+        "┏┅<tg-emoji emoji-id=\"6032594876506312598\">👥</tg-emoji>┅/ <b>Лимит участников</b> /\n"
         "┋\n"
         "┣ Сколько участников нужно для завершения?\n"
         "┋\n"
@@ -53,14 +53,14 @@ TEXTS = {
     ),
     "giveaway_custom_option_btn": "СВОЙ ВАРИАНТ",
     "giveaway_enter_winners_count": (
-        "┏┅<tg-emoji emoji-id=\"5805553606635559688\">🏆</tg-emoji>┅ / <b>Количество победителей</b> /\n"
+        "┏┅<tg-emoji emoji-id=\"5805553606635559688\">🏆</tg-emoji>┅/ <b>Количество победителей</b> /\n"
         "┋\n"
         "┣ Сколько будет победителей?\n"
         "┋\n"
         "┗┅┅┅/ <b>Выберите или введите число:</b> /"
     ),
     "giveaway_enter_prizes": (
-        "┏┅<tg-emoji emoji-id=\"5235695112419303615\">🎁</tg-emoji>┅ / <b>Призы</b> /\n"
+        "┏┅<tg-emoji emoji-id=\"5235695112419303615\">🎁</tg-emoji>┅/ <b>Призы</b> /\n"
         "┋\n"
         "┣ Введите призы для победителей.\n"
         "┣ Каждый приз с новой строки.\n"
@@ -69,7 +69,7 @@ TEXTS = {
     ),
     "giveaway_confirm_prizes_btn": "ПОДТВЕРДИТЬ ПРИЗЫ",
     "giveaway_select_access": (
-        "┏┅<tg-emoji emoji-id=\"5258476306152038031\">🔒</tg-emoji>┅ / <b>Режим доступа</b> /\n"
+        "┏┅<tg-emoji emoji-id=\"5258476306152038031\">🔒</tg-emoji>┅/ <b>Режим доступа</b> /\n"
         "┋\n"
         "┣ Кто может участвовать в розыгрыше?\n"
         "┋\n"
@@ -78,14 +78,14 @@ TEXTS = {
     "giveaway_access_all_btn": "ПУБЛИЧНЫЙ",
     "giveaway_access_whitelist_btn": "ВАЙТЛИСТ (ЮЗЕРЫ/ID)",
     "giveaway_enter_whitelist": (
-        "┏┅<tg-emoji emoji-id=\"5258476306152038031\">📝</tg-emoji>┅ / <b>Вайтлист</b> /\n"
+        "┏┅<tg-emoji emoji-id=\"5258476306152038031\">📝</tg-emoji>┅/ <b>Вайтлист</b> /\n"
         "┋\n"
         "┣ Отправьте список разрешенных ID пользователей или @usernames.\n"
         "┋\n"
         "┗┅┅┅/ <b>Пример:</b> @user1 12345678 /"
     ),
     "giveaway_preview_title": (
-        "┏┅<tg-emoji emoji-id=\"5258254475386167466\">🖼️</tg-emoji>┅ / <b>ПРЕДПРОСМОТР</b> /\n"
+        "┏┅<tg-emoji emoji-id=\"5258254475386167466\">🖼️</tg-emoji>┅/ <b>ПРЕДПРОСМОТР</b> /\n"
         "┋\n"
         "{preview}\n"
         "┋\n"
@@ -101,7 +101,7 @@ TEXTS = {
     "giveaway_start_btn": "START",
     "giveaway_join_btn": "ПОДПИСАТЬСЯ",
     "giveaway_created": (
-        "┏┅<tg-emoji emoji-id=\"5260726538302660868\">✅</tg-emoji>┅ / <b>Розыгрыш успешно создан!</b> /\n"
+        "┏┅<tg-emoji emoji-id=\"5260726538302660868\">✅</tg-emoji>┅/ <b>Розыгрыш успешно создан!</b> /\n"
         "┋\n"
         "┗┅┅┅/ <b>Он будет автоматически опубликован в группе</b> /"
     ),
@@ -124,7 +124,7 @@ TEXTS = {
     "giveaway_success_join": "Вы успешно присоединились к розыгрышу!",
     "giveaway_finished": "Розыгрыш уже завершен.",
     "giveaway_no_participants_results": (
-        "┏┅⋐[ ◍ _◍ ]っ┅<tg-emoji emoji-id=\"5235695112419303615\">🎁</tg-emoji>┅ <b>/ {title} /</b>\n"
+        "┏┅⋐[ ◍ _◍ ]っ┅<tg-emoji emoji-id=\"5235695112419303615\">🎁</tg-emoji>┅<b>/ {title} /</b>\n"
         "┣ <b>Unfortunately, there were no humans...</b>\n"
         "┋\n"
         "┣ <b>GIVEAWAY</b>\n"
@@ -132,7 +132,7 @@ TEXTS = {
         "┗┅┅┅/ #NOTAPES /"
     ),
     "giveaway_winners_results": (
-        "┏┅⋐[ ◍ _◍ ]っ┅<tg-emoji emoji-id=\"5235695112419303615\">🎁</tg-emoji>┅ <b>/ {title} /</b>\n"
+        "┏┅⋐[ ◍ _◍ ]っ┅<tg-emoji emoji-id=\"5235695112419303615\">🎁</tg-emoji>┅<b>/ {title} /</b>\n"
         "┣ <b>GAME OVER!</b>\n"
         "┋\n"
         "{winners_list}"
@@ -142,12 +142,12 @@ TEXTS = {
         "┗┅┅┅/ #NOTAPES /"
     ),
     "giveaway_finished_notify": (
-        "┏┅<tg-emoji emoji-id=\"5258096772776991776\">⚙️</tg-emoji>┅ / <b>Розыгрыш {title} завершен!</b> /\n"
+        "┏┅<tg-emoji emoji-id=\"5258096772776991776\">⚙️</tg-emoji>┅/ <b>Розыгрыш {title} завершен!</b> /\n"
         "┋\n"
         "┗┅┅┅/ <b>Результаты опубликованы в группе</b> /"
     ),
     "giveaway_history_title": (
-        "┏┅⋐[ ◍ _◍ ]っ┅<tg-emoji emoji-id=\"5235695112419303615\">🎁</tg-emoji>┅ / <b>Созданные розыгрыши</b> /\n"
+        "┏┅⋐[ ◍ _◍ ]っ┅<tg-emoji emoji-id=\"5235695112419303615\">🎁</tg-emoji>┅/ <b>Созданные розыгрыши</b> /\n"
         "┋\n"
         "{content}\n"
         "┋\n"
