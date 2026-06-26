8a47c69b44da964757014686cb8167ea5301dd4a
jammm <nikitacirskov0@gmail.com>
2026-06-26 10:08:38 +0300

    Fix formatting in Russian wallet texts

--- locales/ru/wallet.py
+++ locales/ru/wallet.py
@@ -1,31 +1,31 @@
 TEXTS = {
     "wallet_connected": (
-        "┏┅<tg-emoji emoji-id=\"5258204546391351475\">💰</tg-emoji>┅ / <b>Кошелек подключен</b> /\n"
+        "┏┅<tg-emoji emoji-id=\"5258204546391351475\">💰</tg-emoji>┅/ <b>Кошелек подключен</b> /\n"
         "┋\n"
         "┣ <code>{address}</code>\n"
         "┋\n"
         "┗┅┅┅/ <b>Вы можете отключить этот кошелек и привязать новый, если необходимо</b> /"
     ),
     "wallet_connect": (
-        "┏┅<tg-emoji emoji-id=\"5258204546391351475\">💰</tg-emoji> <b>Подключить кошелек</b> /\n"
+        "┏┅<tg-emoji emoji-id=\"5258204546391351475\">💰</tg-emoji>┅/ <b>Подключить кошелек</b> /\n"
         "┋\n"
         "┣ Подключите свой TON-кошелек, чтобы получить доступ к функциям для холдеров и розыгрышам.\n"
         "┋\n"
-        "┗┅┅┅/ <b>Выберите предпочтительный кошелек ниже:</b> /"
+        "┗┅┅┅/ <b>Выберите предпочтительный кошелек ниже</b> /"
     ),
     "wallet_disconnect_btn": "ОТКЛЮЧИТЬ",
     "wallet_connect_btn": "ПОДКЛЮЧИТЬ КОШЕЛЕК",
     "wallet_back_btn": "НАЗАД",
     "wallet_select_wallet": "<b>Выберите кошелек:</b>",
     "wallet_connection_title": (
-        "┏┅<tg-emoji emoji-id=\"5258204546391351475\">💰</tg-emoji>┅ / Подключение <b>{wallet_name}</b> /\n"
+        "┏┅<tg-emoji emoji-id=\"5258204546391351475\">💰</tg-emoji>┅/ Подключение <b>{wallet_name}</b> /\n"
         "┋\n"
         "┗┅┅┅/ <b>Нажмите кнопку ниже и подтвердите подключение внутри</b> {wallet_name}"
     ),
     "wallet_open_wallet_btn": "ОТКРЫТЬ КОШЕЛЕК",
     "wallet_cancel_btn": "ОТМЕНА",
     "wallet_success_title": (
-        "┏┅<tg-emoji emoji-id=\"5258204546391351475\">💰</tg-emoji>┅ / <b>Успешно!</b> /\n"
+        "┏┅<tg-emoji emoji-id=\"5258204546391351475\">💰</tg-emoji>┅/ <b>Успешно!</b> /\n"
         "┋\n"
         "┗┅┅┅/ <b>Ваш кошелек привязан:</b> <code>{address}</code>"
     ),
