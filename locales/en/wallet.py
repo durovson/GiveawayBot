398b6b9135e07ee1a90e81c16657c44cac795e60
jammm <nikitacirskov0@gmail.com>
2026-06-26 10:02:56 +0300

    Fix formatting of wallet connection messages

--- locales/en/wallet.py
+++ locales/en/wallet.py
@@ -1,13 +1,13 @@
 TEXTS = {
     "wallet_connected": (
-        "┏┅<tg-emoji emoji-id=\"5258204546391351475\">💰</tg-emoji>┅ / <b>Wallet Connected</b> /\n"
+        "┏┅<tg-emoji emoji-id=\"5258204546391351475\">💰</tg-emoji>┅/ <b>Wallet Connected</b> /\n"
         "┋\n"
         "┣ <code>{address}</code>\n"
         "┋\n"
         "┗┅┅┅/ <b>You can disconnect this wallet and link a new one if needed</b> /"
     ),
     "wallet_connect": (
-        "┏┅<tg-emoji emoji-id=\"5258204546391351475\">💰</tg-emoji> <b>Connect Wallet</b> /\n"
+        "┏┅<tg-emoji emoji-id=\"5258204546391351475\">💰</tg-emoji>┅/ <b>Connect Wallet</b> /\n"
         "┋\n"
         "┣ Connect your TON wallet to access holder features and giveaways.\n"
         "┋\n"
@@ -18,14 +18,14 @@ TEXTS = {
     "wallet_back_btn": "BACK",
     "wallet_select_wallet": "<b>Select your wallet:</b>",
     "wallet_connection_title": (
-        "┏┅<tg-emoji emoji-id=\"5258204546391351475\">💰</tg-emoji>┅ / {wallet_name} <b>Connection</b> /\n"
+        "┏┅<tg-emoji emoji-id=\"5258204546391351475\">💰</tg-emoji>┅/ {wallet_name} <b>Connection</b> /\n"
         "┋\n"
         "┗┅┅┅/ <b>Tap the button below and confirm connection inside</b> {wallet_name}"
     ),
     "wallet_open_wallet_btn": "OPEN WALLET",
     "wallet_cancel_btn": "CANCEL",
     "wallet_success_title": (
-        "┏┅<tg-emoji emoji-id=\"5258204546391351475\">💰</tg-emoji>┅ / <b>Success!</b> /\n"
+        "┏┅<tg-emoji emoji-id=\"5258204546391351475\">💰</tg-emoji>┅/ <b>Success!</b> /\n"
         "┋\n"
         "┗┅┅┅/ <b>Your wallet has been linked:</b> <code>{address}</code>"
     ),
